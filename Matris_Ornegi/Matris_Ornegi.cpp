715a3937d06c118a8edb922a202da874c0eb3126
Cesasie <Cesasie@DESKTOP-4F3U2I3>
2023-04-22 17:23:20 +0300

    Proje dosyası ekle.

---
+++ Matris_Ornegi/Matris_Ornegi.cpp
@@ -0,0 +1,28 @@
+﻿//ters kosegen (anti-diagon) 1 olan ve diger butun elemanlari 0 olan matrisi ekrana bastiriniz.
+
+#include <iostream>
+using namespace std;
+
+
+//0001
+//0010
+//0100
+//1000
+
+int main() {
+	int b;
+	cout << "boyutu giriniz :";
+	cin >> b;
+
+	for (int i = 0; i < b; i++) { // satir sayisi
+		for (int j = 0; j < b; j++) { // kolon sayisi
+			if (i+j == b-1)
+				cout << "1";
+			else
+				cout << "0";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
